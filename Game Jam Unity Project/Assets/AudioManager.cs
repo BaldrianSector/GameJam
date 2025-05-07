0a05242e8768b21e386fa1b3ad6e33f700b3ebc5
Baldrian Sector <baldriansector@gmail.com>
2025-05-07 14:21:47 +0200

    Add Audio Manager Script + Sound

---
+++ Game Jam Unity Project/Assets/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("*---------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+
+    [Header("*---------- Audio Clip -----------")]
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip checkpoint;
+    public AudioClip wallTouch; 
+    public AudioClip portalin; 
+    public AudioClip portalout;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
